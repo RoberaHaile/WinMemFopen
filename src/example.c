acb6880d61f4016b8548ba07f0d54c1f8b1aaa5c
RoberaHaile <robera555@gmail.com>
2020-04-24 18:22:11 +0300

    modified:   README.md 	modified:   src/example.c

--- src/example.c
+++ src/example.c
@@ -4,20 +4,25 @@
 #include "WinMemFopen.h"
 
 int main() {
-	char fileBuffer[] = "u are gonna miss my love, by lou rawls. XXXX"; 
-	handle_struct* trialHandle = NULL;
+	/* targetBuffer[] will be the memory buffer which will be opened as a file. 
+	* readBuf[] will be our buffer into which will read from our target buffer 
+	* writeBuf[] will be our string which we will write into our target buffer. 
+	*/ 
+
+	char targetBuffer[] = "u are gonna miss my love, by lou rawls. XXXX"; 
+	handle_struct* myHandle = NULL;
 	char readBuf[5]; 
 	char*writeStr = "FFFFF"; 
 	LONG newOffset = 0; 
 
-	trialHandle = FsOpenMem(fileBuffer, 44); 
-	memset(readBuf, 0, 5); 
-	FsReadFromMemH(trialHandle, readBuf, 5);
+	myHandle = FsOpenMem(targetBuffer, 44); 				// open targetBuffer[] and represent it with myHandle->fHandle.
+	memset(readBuf, 0, 5);		 
+	FsReadFromMemH(myHandle, readBuf, 5);					// readBuf will be "u are". 
 	memset(readBuf, 0, 5);
-	FsReadFromMemH(trialHandle, readBuf, 5);
-	newOffset = FsSeekMemH(trialHandle, 3, SEEK_CUR); 
-	FsWriteToMemH(trialHandle, (unsigned char*)writeStr, 5);
-	FsCloseM(&trialHandle); 
+	FsReadFromMemH(myHandle, readBuf, 5);					// reaBuf will be " gonn". 
+	newOffset = FsSeekMemH(myHandle, 3, SEEK_CUR); 			// myHandle->fHandle new offset will be 13 relative from the beginning. 
+	FsWriteToMemH(myHandle, (unsigned char*)writeStr, 5);	// targetBuffer[] will be "u are gonna mFFFFFy love, by lou rawls. XXXX"
+	FsCloseM(&myHandle); 									// close and free myHandle struct and its member myHandle->mmioInfo. 
 
 	return 0; 
 }
